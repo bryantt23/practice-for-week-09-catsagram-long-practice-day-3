00487a74eb3ee12ba871adfc24852f8340268acb
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-21 01:45:35 -0700

    initial

--- public/index.js
+++ public/index.js
@@ -3,19 +3,19 @@ import { createScoreContainer } from './score.js';
 import { createCommentSection } from './comments.js';
 
 const initializePage = () => {
-    // Create container
-    const container = document.createElement("section");
-    container.className = "container";
-    container.style.display = "flex";
-    container.style.flexDirection = "column";
-    container.style.alignItems = "center";
-    container.style.marginTop = "20px";
-    document.body.appendChild(container);
+  // Create container
+  const container = document.createElement('section');
+  container.className = 'container';
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+  container.style.alignItems = 'center';
+  container.style.marginTop = '20px';
+  document.body.appendChild(container);
 };
 
 window.onload = () => {
-    initializePage();
-    createMainContent();
-    createScoreContainer();
-    createCommentSection();
+  initializePage();
+  createMainContent();
+  createScoreContainer();
+  createCommentSection();
 };
